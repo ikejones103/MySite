9c8ac1905aad7a7b2d3e5da59a324dac2cde14ef
Isaac Jones <108473034+ikejones103@users.noreply.github.com>
2024-02-09 12:23:34 -0600

    Add files via upload

---
+++ project/js/main.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button1 = document.getElementById("button1");
+    const button2 = document.getElementById("button2");
+    const button3 = document.getElementById("button3");
+    const button4 = document.getElementById("button4");
+    const button5 = document.getElementById("button5");
+    const button6 = document.getElementById("button6");
+    const button7 = document.getElementById("button7");
+
+    button1.addEventListener("click", function() {
+        window.location.href = "https://www.figma.com/proto/kczEiA9ku1bKqoTBuRtzKA?node-id=60-114&starting-point-node-id=60:114&locale=en";
+    });
+
+    button2.addEventListener("click", function() {
+        window.location.href = "https://xd.adobe.com/view/82b701aa-a4e2-4e99-b9a9-ffd286890b04-d7e4/";
+    });
+
+    button3.addEventListener("click", function() {
+        window.location.href = "https://xd.adobe.com/view/72abb00a-8b73-47c8-a962-1896d7109152-27f8/";
+    });
+    
+    button4.addEventListener("click", function() {
+        window.location.href = "https://www.linkedin.com/in/isaac-jones-ike/";
+    });
+    
+    button5.addEventListener("click", function() {
+        window.location.href = "https://twitter.com/ijones392262";
+    });
+    
+    button6.addEventListener("click", function() {
+        window.location.href = "https://www.facebook.com/profile.php?id=61556316476312";
+    });
+
+    button7.addEventListener("click", function() {
+        window.location.href = "https://docs.google.com/document/d/1RJj295IjVF2cCecSifi65LSRXxSIJdc9/edit?usp=sharing&ouid=100116557588710267481&rtpof=true&sd=true";
+    });
+});
